920460da5f491e1543e0bbd4e17edb4c38dab84e
Raphael Macedo de Carvalho <raphaelbluteau@gmail.com>
2019-02-18 12:24:52 -0300

    Adding unit test for receiver

--- src/main/java/com/github/cafecomjavanaresource/todo/listener/Receiver.java
+++ src/main/java/com/github/cafecomjavanaresource/todo/listener/Receiver.java
@@ -1,15 +1,17 @@
 package com.github.cafecomjavanaresource.todo.listener;
 
 import com.github.cafecomjavanaresource.todo.message.data.TodoMessage;
+import lombok.extern.java.Log;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@Log
 public class Receiver {
 
     @JmsListener(destination = "todo-channel", containerFactory = "myFactory")
     public void receiveMessage(TodoMessage todo) {
-        System.out.println(String.format("#### Received todo: < %s >", todo));
+        log.info(String.format("#### Received todo: < %s >", todo));
     }
 
 }
